fd50da3e7a81dc21b66420965a9162b1139400af
david-1638634303 <davidferreiram95@gmail.com>
2022-01-13 15:16:10 -0300

    Head and material theme provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import theme from '../src/theme';
+
+function MyApp({ Component, pageProps }: any) {
+  return (
+    <>
+      <Head>
+        <title>Notification sys</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Head>
+
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </MuiThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
