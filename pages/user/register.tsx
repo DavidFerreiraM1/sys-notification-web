e7b3949eca9cb2df99a58bb4d074d3a94ac77fb6
david-1638634303 <davidferreiram95@gmail.com>
2022-01-14 10:15:06 -0300

    register page added

---
+++ pages/user/register.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { Register } from '../../src//features';
+
+const RegisterPage: NextPage = () => {
+  return <Register />
+}
+
+export default RegisterPage;
