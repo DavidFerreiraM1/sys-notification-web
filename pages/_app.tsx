5f64bc068afbf32783d3eeec4de0c9692b33c72a
david-1638634303 <davidferreiram95@gmail.com>
2022-01-16 20:49:08 -0300

    ubuntu font added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import '../styles/globals.css'
 import Head from 'next/head'
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
@@ -10,7 +11,10 @@ function MyApp({ Component, pageProps }: any) {
       <Head>
         <title>Notification sys</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Head>
 
       <MuiThemeProvider theme={theme}>
