6a92e7ea504b074eba9d093536ced3b6be44dbdf
david-1638634303 <davidferreiram95@gmail.com>
2022-01-16 23:30:00 -0300

    web push form added on context

--- src/features/new-application/context.tsx
+++ src/features/new-application/context.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { ComponentWithChildrenProps } from '../../utils/interfaces/with-children-props';
 import { AppChannelFormOpennedTypes, NewAppFormValueProvider } from './interfaces';
+import { newAppForm, newWebPushForm } from './utils';
 
 const NewAppFormContext = React.createContext({} as NewAppFormValueProvider);
 
 export function NewAppFormProvider(props: ComponentWithChildrenProps) {
   const [appChannelFormOpenned, setChannelFormOpenned] = React.useState<AppChannelFormOpennedTypes>('');
-  const [appForm, setAppForm] = React.useState({
-    id: '',
-    name: '',
-  });
+  const [appForm, setAppForm] = React.useState(newAppForm);
+  const [webPushForm, setNewWebPushForm, ] = React.useState(newWebPushForm);
 
   const appChannelFormOpennedValueHandler = React.useCallback((value: AppChannelFormOpennedTypes) => {
     setChannelFormOpenned(value);
@@ -19,13 +18,26 @@ export function NewAppFormProvider(props: ComponentWithChildrenProps) {
     setAppForm({ ...appForm, [key]: value });
   }, [appForm]);
 
+  const webPushValueHandler = React.useCallback(
+    (option: 'site' | 'allowNotification' | 'welcomeNotification', key: string) => (value: string | number) => {
+    setNewWebPushForm({
+      ...webPushForm,
+      [option]: {
+        ...webPushForm[option],
+        [key]: value
+      },
+    });
+  }, [webPushForm]);
+
   return (
     <NewAppFormContext.Provider
       value={{
         appForm,
         appChannelFormOpenned,
         appChannelFormOpennedValueHandler,
-        appFormValueHandler
+        appFormValueHandler,
+        webPushForm,
+        webPushValueHandler
       }}
     >
       {props.children}
