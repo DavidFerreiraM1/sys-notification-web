60d59c8ac9efa36c19469e05b50f64df4a6763d0
david-1638634303 <davidferreiram95@gmail.com>
2022-01-16 23:30:53 -0300

    web push form type created

--- src/features/new-application/interfaces.ts
+++ src/features/new-application/interfaces.ts
@@ -7,7 +7,29 @@ export type AppChannelFormOpennedTypes = 'web-push' | 'sms' | 'email' | '';
 
 export interface NewAppFormValueProvider {
   appForm: NewAppFormType;
+  webPushForm: WebPushFormType;
   appChannelFormOpenned: AppChannelFormOpennedTypes;
   appChannelFormOpennedValueHandler(value: AppChannelFormOpennedTypes): void;
   appFormValueHandler(key: string): (value: string) => void;
+  webPushValueHandler(option: 'site' | 'allowNotification' | 'welcomeNotification', key: string): (value: string | number) => void;
 };
+
+export interface WebPushFormType {
+  site: {
+    name: string;
+    address: string;
+    urlIcon: string;
+  };
+  allowNotification: {
+    messageText: string;
+    allowButtonText: string;
+    denyButtonText: string;
+  };
+  welcomeNotification: {
+    messageTitle: string;
+    messageText: string;
+    enableUrlRedirect: number;
+    urlRedirect: string;
+  };
+}
+
--- src/features/new-application/utils.ts
+++ src/features/new-application/utils.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 import { createShapeValidations } from '../../utils/validations/form-validation';
-import { NewAppFormType } from './interfaces';
+import { NewAppFormType, WebPushFormType } from './interfaces';
 
 export const newAppForm: NewAppFormType = {
   id: '',
@@ -10,3 +10,22 @@ export const newAppForm: NewAppFormType = {
 export const newAppShapeValidations = createShapeValidations({
   name: yup.string().required('Este campo é obrigatório!'),
 });
+
+export const newWebPushForm: WebPushFormType = {
+  site: {
+    name: '',
+    address: '',
+    urlIcon: '',
+  },
+  allowNotification: {
+    messageText: '',
+    allowButtonText: '',
+    denyButtonText: '',
+  },
+  welcomeNotification: {
+    messageTitle: '',
+    messageText: '',
+    enableUrlRedirect: 0,
+    urlRedirect: '',
+  },
+}
