80643b5c9ff54081fb3005fa1b8507c55a5e973f
david-1638634303 <davidferreiram95@gmail.com>
2022-01-17 21:03:29 -0300

    container page added on new application

--- src/features/new-application/index.tsx
+++ src/features/new-application/index.tsx
@@ -21,6 +21,7 @@ import { AppChannelFormOpennedTypes } from './interfaces';
 import { WebPushForm } from './web-push';
 import { SmsForm } from './sms';
 import { EmailForm } from './email';
+import { ContainerPage } from '../../shared';
 
 function NewAppFormComponent() {
   const classes = newAppStyles();
@@ -58,150 +59,152 @@ function NewAppFormComponent() {
   [appChannelFormOpenned]);
 
   return (
-    <Grid
-      container
-      className={classes.root}
-    >
-      <form>
+    <ContainerPage>
+      <Grid
+        container
+        className={classes.root}
+      >
+        <form>
+          <Grid
+            container
+            className={classes.formContainer}
+          >
+            <Grid item xs={12} sm={12} md={8} lg={8}>
+              <Typography
+                component="h2"
+                variant="h6"
+              >
+                Novo Aplicativo
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={8} lg={8}>
+              <Box className="form-control">
+                <TextField
+                  fullWidth
+                  id="app-name"
+                  color="primary"
+                  label="Nome do aplicativo"
+                  placeholder="Informe o nome de indentificação do aplicativo"
+                  type="text"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                  value={appForm.name}
+                  onChange={changeValueOnAppForm}
+                  error={errors?.name}
+                  helperText={errors?.name && errors.name}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} lg={4}>
+              <Box
+                className="create-app-btn-area form-control"
+              >
+                <Button
+                  className="create-app-btn"
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  onClick={onSubmit}
+                  fullWidth
+                >
+                  Criar Aplicativo
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </form>
+        <Grid xs={12} sm={12} md={12} lg={12}>
+          <Box className={classes.dividerArea}>
+            <Divider className="divider" />
+          </Box>
+        </Grid>
         <Grid
-          container
-          className={classes.formContainer}
+            container
+            className={classes.formContainer}
         >
           <Grid item xs={12} sm={12} md={8} lg={8}>
             <Typography
               component="h2"
               variant="h6"
             >
-              Novo Aplicativo
+              Tipos de canais
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={12} md={8} lg={8}>
-            <Box className="form-control">
-              <TextField
-                fullWidth
-                id="app-name"
-                color="primary"
-                label="Nome do aplicativo"
-                placeholder="Informe o nome de indentificação do aplicativo"
-                type="text"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                variant="outlined"
-                value={appForm.name}
-                onChange={changeValueOnAppForm}
-                error={errors?.name}
-                helperText={errors?.name && errors.name}
-              />
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Box component="ul" className={classes.ulServices}>
+              <Box component="li" className={classes.serviceTypeRoot}>
+                <Box
+                  component="button"
+                  className="content"
+                  onClick={appChannelFormOpennedValueChange('email')}
+                >
+                  <Box className="icon-area">
+                    <EmailIcon />
+                  </Box>
+                  <Box className="service-type-name">
+                    <Typography component="span">
+                      E-mail -
+                    </Typography>
+                    <Typography component="span" className="status-service">
+                      (Configurar)
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Collapse in={appChannelFormOpenned === 'email'}>
+                <EmailForm />
+              </Collapse>
+              <Box component="li" className={classes.serviceTypeRoot}>
+                <Box
+                  component="button"
+                  className="content"
+                  onClick={appChannelFormOpennedValueChange('sms')}
+                >
+                  <Box className="icon-area">
+                    <SmsIcon />
+                  </Box>
+                  <Box className="service-type-name">
+                    <Typography component="span">
+                      SMS -
+                    </Typography>
+                    <Typography component="span" className="status-service">
+                      (Configurar)
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Collapse in={appChannelFormOpenned === 'sms'}>
+                <SmsForm />
+              </Collapse>
+              <Box component="li" className={classes.serviceTypeRoot}>
+                <Box
+                  component="button"
+                  className="content"
+                  onClick={appChannelFormOpennedValueChange('web-push')}
+                >
+                  <Box className="icon-area">
+                    <WebIcon />
+                  </Box>
+                  <Box className="service-type-name">
+                    <Typography component="span">
+                      Web Push -
+                    </Typography>
+                    <Typography component="span" className="status-service">
+                      (Configurar)
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Collapse in={appChannelFormOpenned === 'web-push'}>
+                <WebPushForm />
+              </Collapse>
             </Box>
           </Grid>
-          <Grid item xs={12} sm={12} md={4} lg={4}>
-            <Box
-              className="create-app-btn-area form-control"
-            >
-              <Button
-                className="create-app-btn"
-                variant="contained"
-                color="primary"
-                size="large"
-                onClick={onSubmit}
-                fullWidth
-              >
-                Criar Aplicativo
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </form>
-      <Grid xs={12} sm={12} md={12} lg={12}>
-        <Box className={classes.dividerArea}>
-          <Divider className="divider" />
-        </Box>
-      </Grid>
-      <Grid
-          container
-          className={classes.formContainer}
-      >
-        <Grid item xs={12} sm={12} md={8} lg={8}>
-          <Typography
-            component="h2"
-            variant="h6"
-          >
-            Tipos de canais
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12}>
-          <Box component="ul" className={classes.ulServices}>
-            <Box component="li" className={classes.serviceTypeRoot}>
-              <Box
-                component="button"
-                className="content"
-                onClick={appChannelFormOpennedValueChange('email')}
-              >
-                <Box className="icon-area">
-                  <EmailIcon />
-                </Box>
-                <Box className="service-type-name">
-                  <Typography component="span">
-                    E-mail -
-                  </Typography>
-                  <Typography component="span" className="status-service">
-                    (Configurar)
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
-            <Collapse in={appChannelFormOpenned === 'email'}>
-              <EmailForm />
-            </Collapse>
-            <Box component="li" className={classes.serviceTypeRoot}>
-              <Box
-                component="button"
-                className="content"
-                onClick={appChannelFormOpennedValueChange('sms')}
-              >
-                <Box className="icon-area">
-                  <SmsIcon />
-                </Box>
-                <Box className="service-type-name">
-                  <Typography component="span">
-                    SMS -
-                  </Typography>
-                  <Typography component="span" className="status-service">
-                    (Configurar)
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
-            <Collapse in={appChannelFormOpenned === 'sms'}>
-              <SmsForm />
-            </Collapse>
-            <Box component="li" className={classes.serviceTypeRoot}>
-              <Box
-                component="button"
-                className="content"
-                onClick={appChannelFormOpennedValueChange('web-push')}
-              >
-                <Box className="icon-area">
-                  <WebIcon />
-                </Box>
-                <Box className="service-type-name">
-                  <Typography component="span">
-                    Web Push -
-                  </Typography>
-                  <Typography component="span" className="status-service">
-                    (Configurar)
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
-            <Collapse in={appChannelFormOpenned === 'web-push'}>
-              <WebPushForm />
-            </Collapse>
-          </Box>
         </Grid>
       </Grid>
-    </Grid>
+    </ContainerPage>
   );
 }
 
