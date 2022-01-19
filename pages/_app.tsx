d00d44db94ff0ddc9dfa371f53e1d24b75db95f5
david-1638634303 <davidferreiram95@gmail.com>
2022-01-18 23:05:26 -0300

    fix crossOrigin name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: any) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Head>
 
